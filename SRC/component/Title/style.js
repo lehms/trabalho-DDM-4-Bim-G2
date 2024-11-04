48cc85c353c0fbd74eb2fa0da3907033344a3fdc
lehms <leticia.silva2096@etec.sp.gov.br>
2024-11-04 11:56:09 -0300

    estilo do titulo

---
+++ SRC/component/Title/style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    BoxTitle: {
+    alignItems: "center",
+    },
+    texTitle: {
+    color: "#EA3829",
+    },
+   });
+   export default styles
